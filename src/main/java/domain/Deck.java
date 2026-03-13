3a3657bc1f56f6cf8146d4e49414e624b434fe94
pepenacho <herreragrej@proton.me>
2026-03-13 19:05:13 -0300

    realistic shuffle

--- src/main/java/domain/Deck.java
+++ src/main/java/domain/Deck.java
@@ -5,12 +5,15 @@ import domain.enums.Suit;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Deck {
   private final List<Card> cards = new ArrayList<>(52);
+  private final static Random random = new Random();
 
   public Deck() {
     createDeck();
+    shuffle(cards, random.nextInt(5, 8));
   }
 
   private void createDeck() {
@@ -18,6 +21,17 @@ public class Deck {
       for (Rank rank : Rank.values()) {
         cards.add(new Card(suit.getSymbol(), rank.getSymbol()));
       }
+    }
+  }
+
+  public void shuffle(List<Card> cards, int repeat) {
+    for (int i = 0; i < repeat; i++) {
+      int min = random.nextInt(cards.size()/2 + 1);
+      int max = random.nextInt(cards.size()/2 + 10, cards.size());
+      List<Card> sub = new ArrayList<>(cards.subList(min, max));
+      cards.removeAll(sub);
+      int startIndex = random.nextInt(cards.size());
+      cards.addAll(startIndex, sub);
     }
   }
 
